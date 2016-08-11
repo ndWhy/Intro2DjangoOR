263ed39ca7ee954935e3aade327ee8e023f19ae9
ndWhy <ndwhy@gmail.com>
2016-08-11 23:15:59 +0300

    Add ModelForm for creating new book review requst

--- readit/urls.py
+++ readit/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from books.views import (AuthorDetail, AuthorList, BookDetail, 
-						list_books, review_book, review_books)
+						list_books, review_book, ReviewList)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
-    url(r'^review/$', review_books, name='review-books'),
+    url(r'^review/$', ReviewList.as_view(), name='review-books'),
 	url(r'^review/(?P<pk>[-\w]+)/$', review_book, name='review-book'),
 
 ]
